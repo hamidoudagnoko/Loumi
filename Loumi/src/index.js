09adaf274819c7fef177f0afcb2b084fd780ccd2
smarthamidou <77022730+smarthamidou@users.noreply.github.com>
2021-01-06 09:14:05 +0100

    Update index.js

--- Loumi/src/index.js
+++ Loumi/src/index.js
@@ -6,7 +6,17 @@ import App from "./App";
 const rootElement = document.getElementById("root");
 ReactDOM.render(
   <React.StrictMode>
+    <h1> Colocation de 4 pers </h1>
+
     <App />
+    <tr>Elie girard (CDI)</tr>
+    <tr>Elie girard (profession liberale)</tr>
+    <h1> Revenu mensuel moyen </h1>
+    <h2> garant (5206)</h2>
+    <h2> candidat (6700)</h2>
+    <td>
+      <input onclick="myFunction()" type="button" value="Voir plus" />
+    </td>
   </React.StrictMode>,
   rootElement
 );
